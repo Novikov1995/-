7ecbc9367cbb8cc52537f60f83fda5355ce6cc9c
Novikov1995 <novikov.anton.kyiv@gmail.com>
2022-05-18 23:28:07 +0800

    govno ti?
    da ya

---
+++ app.js
@@ -0,0 +1,9 @@
+let amount = 299
+const fistingprice = 300
+const slaveprice = 0
+let rest = amount - fistingprice
+if (rest >= slaveprice)
+alert (`let celebrate and suck some dicks`)
+else
+console.log(`I do not have enough money`)
+document.write (`I do not have enough money`)
